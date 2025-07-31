3b58829a5af85bcd604e2c992e08914bd3dde32e
EduardoAquinta <eddie.ashton@madetech.com>
2025-07-31 12:20:11 +0100

    fix: testing and dependency uplift

--- test/web/trainingitems.spec.js
+++ test/web/trainingitems.spec.js
@@ -1,19 +1,30 @@
 import request from 'supertest'
 import { app } from '../../src/app.js'
 
-describe.skip('/training-items', function () {
+describe('/training-items', function () {
   // Admin only
-  it('should load list of training items', function () {
+  it('should not load list of training items', function () {
     return request(app)
       .get('/training-items')
       .set('SMART-USER', 'jim@enterprise.ufp')
       .expect(403)
   })
 
-  // it('should load a training items', function () {
-  //   return request(app)
-  //     .get('/training-items/349776ed-7faf-42eb-beab-81529e8cd69b')
-  //     .set('SMART-USER', 'jim@enterprise.ufp')
-  //     .expect(200)
-  // })
+  it('should load list of training items', function () {
+    return request(app)
+      .get('/training-items')
+      .set('SMART-USER', 'mca.ab@service.dev.smart.mcga.uk')
+      .expect(200)
+      .then(data => {
+        expect(data.text).toContain('Advanced Fire Fighting (STCW code Table A-VI/3)')
+      })
+
+  })
+
+  it('should load a training items', function () {
+    return request(app)
+      .get('/training-items/26b7a2ea-0932-437c-96c2-e8ab4cce7696')
+      .set('SMART-USER', 'mca.ab@service.dev.smart.mcga.uk')
+      .expect(200)
+  })
 })
