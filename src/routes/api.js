b0146fc577546f3b8f747654ec84de12172b8212
RaoKonda <raokonda@RaoKondas-MacBook-Air.local>
2026-03-13 16:34:11 +0000

    added monthly trainee report UI

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,7 +2,8 @@ import express from 'express'
 import { traineeSearch } from '../controllers/lookups/trainees.js'
 import { useJson } from '../controllers/common.js'
 import { all as allCompanies } from '../controllers/client-companies.mjs'
-import { getContinuingTraineeReport } from '../controllers/reports.js'
+import { getContinuingTraineeReport, getMonthlyTraineeReportSummary } from '../controllers/reports.js'
+
 
 const router = new express.Router()
 
@@ -11,5 +12,7 @@ router.get('/trainees/autocomplete', traineeSearch(), useJson('trainees'))
 router.get('/client-companies', allCompanies, useJson('clientCompanies'))
 
 router.put('/reports/continuing-trainee-report', getContinuingTraineeReport())
+
+router.put('/reports/monthly-trainee-report', getMonthlyTraineeReportSummary())
 
 export default router
