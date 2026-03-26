4cafbc97654fe7f699436759992df0dc1fea9314
RaoKonda <raokonda@RaoKondas-MacBook-Air.local>
2026-03-26 11:41:48 +0000

    Added failure report changes

--- src/routes/api-reports.js
+++ src/routes/api-reports.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { getContinuingTraineeReport, downloadReport, checkReportExists, getMonthlyTraineeReportSummary } from '../controllers/reports.js'
+import { getContinuingTraineeReport, downloadReport, checkReportExists, getMonthlyTraineeReportSummary, getFailuresReport } from '../controllers/reports.js'
 
 const router = new express.Router()
 
 router.put('/continuing-trainee-report', getContinuingTraineeReport())
 router.put('/monthly-trainee-report', getMonthlyTraineeReportSummary())
+router.put('/failures-report', getFailuresReport())
 router.get('/:reportType/download', downloadReport())
 router.head('/:reportType', checkReportExists())
 
