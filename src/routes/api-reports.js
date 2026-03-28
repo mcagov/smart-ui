99308b2a55fea2516312f97ff5ba04d274b32df9
someswarduddi <someswar.duddi@madetech.com>
2026-03-28 22:50:12 +0000

    feat: SOW7-146: Invoice report ui changes

--- src/routes/api-reports.js
+++ src/routes/api-reports.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { getContinuingTraineeReport, downloadReport, checkReportExists, getMonthlyTraineeReportSummary, getFailuresReport } from '../controllers/reports.js'
+import { getContinuingTraineeReport, downloadReport, checkReportExists, getMonthlyTraineeReportSummary, getFailuresReport, getInvoiceReport } from '../controllers/reports.js'
 
 const router = new express.Router()
 
 router.put('/continuing-trainee-report', getContinuingTraineeReport())
 router.put('/monthly-trainee-report', getMonthlyTraineeReportSummary())
 router.put('/failures-report', getFailuresReport())
+router.put('/invoice-report', getInvoiceReport())
 router.get('/:reportType/download', downloadReport())
 router.head('/:reportType', checkReportExists())
 
