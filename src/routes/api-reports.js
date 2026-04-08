80e5673af186bcd6d0854e352109627e70097557
RaoKonda <raokonda@RaoKondas-MacBook-Air.local>
2026-04-08 15:12:54 +0100

    Added trainee demographic report

--- src/routes/api-reports.js
+++ src/routes/api-reports.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getContinuingTraineeReport, downloadReport, checkReportExists, getMonthlyTraineeReportSummary, getFailuresReport, getInvoiceReport } from '../controllers/reports.js'
+import { getContinuingTraineeReport, downloadReport, checkReportExists, getMonthlyTraineeReportSummary, getFailuresReport, getInvoiceReport, getTraineesDemographicReport } from '../controllers/reports.js'
 
 const router = new express.Router()
 
@@ -7,6 +7,7 @@ router.put('/continuing-trainee-report', getContinuingTraineeReport())
 router.put('/monthly-trainee-report', getMonthlyTraineeReportSummary())
 router.put('/failures-report', getFailuresReport())
 router.put('/invoice-report', getInvoiceReport())
+router.put('/trainees-demographic-report', getTraineesDemographicReport())
 router.get('/:reportType/download', downloadReport())
 router.head('/:reportType', checkReportExists())
 
