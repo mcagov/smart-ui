e63b79917763b7c4847f726870724f8683cc9b1e
EduardoAquinta <eddie.ashton@madetech.com>
2026-07-30 14:55:40 +0100

    update: remove space from perms iam role

--- terraform/iam_smarts_ui.tf
+++ terraform/iam_smarts_ui.tf
@@ -69,7 +69,7 @@ data "aws_iam_policy_document" "codeartifact_read_only" {
     effect    = "Allow"
     actions   = [
       "ecr:GetAuthorizationToken",
-      "ecr-public: GetAuthorizationToken"
+      "ecr-public:GetAuthorizationToken"
     ]
     resources = ["*"]
   }
